05ef25e9cf8c30e6980e4405e84b8c0be008b484
Alexandre Celier <50583984+axl6409@users.noreply.github.com>
2023-02-14 12:24:07 +0100

    NEW: insert function and default init datas on installation

--- cookie.php
+++ cookie.php
@@ -74,7 +74,7 @@ function create_db() {
             ),
         );
 
-        $wpdb->insert($cookie_table_name, array(
+        $wpdb->insert($cookie_table_name_config, array(
             'settings_datas' => json_encode($defaults),
             'created_at' => $created_at,
             'updated_at' => $updated_at,
